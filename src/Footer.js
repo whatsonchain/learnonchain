7726d8203107f3e5ae924bd9fa84dd93be5aebda
Liam Missin <l.missin@nchain.com>
2019-05-28 15:16:21 +0100

    centre footer text

--- src/Footer.js
+++ src/Footer.js
@@ -5,24 +5,22 @@ class Footer extends Component {
     return (
       <footer style={{ marginTop: 50 }} className='footer bg-dark text-light pt-3 pb-1 px-3'>
         <div className='container'>
-          <div className='row'>
-            <div className='col-md-7 text-md-right'>
-              <dl id='footer.support'>Support Development of LearnOnChain.com<dd>
-                <div className='text-md-right text-center'>
-                  <div
-                    style={{ display: 'inline-block', maxWidth: 150 }}
-                    className='text-center mb-3 word-wrap monospace'
-                  >
-                    <br />
-                    <span>BSV Address: </span>
-                    <a href='https://whatsonchain.com/address/bitcoincash:qq7su5mghkkamjss3g87g3eejxf8f3excuekujtlev'>
+          <div className='text-center'>
+            <dl id='footer.support'>Support Development of LearnOnChain.com<dd>
+              <div className='text-center'>
+                <div
+                  style={{ display: 'inline-block', maxWidth: 150 }}
+                  className='text-center mb-3 word-wrap monospace'
+                >
+                  <br />
+                  <span className='text-center'>BSV Address: </span>
+                  <a href='https://whatsonchain.com/address/bitcoincash:qq7su5mghkkamjss3g87g3eejxf8f3excuekujtlev'>
                         QR
-                    </a>
-                  </div>
+                  </a>
                 </div>
-              </dd>
-              </dl>
-            </div>
+              </div>
+            </dd>
+            </dl>
           </div>
         </div>
       </footer>
